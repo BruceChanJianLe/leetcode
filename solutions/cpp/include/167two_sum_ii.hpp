eb33dcecca70b32ddb718d0102779b4835915a1f
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2024-10-04 19:07:48 +0800

    wip: two sum ii

---
+++ solutions/cpp/include/167two_sum_ii.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vector>
+
+// Less branches, less prediction, faster execution
+// Second solution
+class Solution {
+public:
+  std::vector<int> twoSum(std::vector<int> &numbers, int target) {
+    int lp = numbers.size() - 1;
+    std::vector<int> index{0, lp};
+    int sum = numbers[index[0]] + numbers[index[1]];
+    while (sum != target)
+    {
+      if (sum > target)
+        --index[1];
+      else if (sum < target)
+        ++index[0];
+      sum = numbers[index[0]] + numbers[index[1]];
+    }
+    return {index[0] + 1, index[1] + 1};
+  }
+};
+
+class FirstSolution {
+public:
+  std::vector<int> twoSum(std::vector<int> &numbers, int target) {
+    int sp{0}, lp;
+    lp = numbers.size() - 1;
+    while (sp != lp)
+    {
+      auto sum = numbers[sp] + numbers[lp];
+      if (sum > target)
+        --lp;
+      else if (sum < target)
+        ++sp;
+      else
+        return {sp + 1, lp + 1};
+    }
+    return {};
+  }
+};
---
+++ solutions/cpp/tests/167two_sum_ii_test.cpp
@@ -0,0 +1,31 @@
+#include "167two_sum_ii.hpp"
+
+#include "gtest/gtest.h"
+
+// 167. Two Sum II Test
+
+struct States
+{
+  std::vector<int> nums;
+  int target;
+  std::vector<int> result;
+};
+
+struct TwoSumIITest : public ::testing::Test, ::testing::WithParamInterface<States>
+{
+  Solution s;
+};
+
+TEST_P(TwoSumIITest, TwoSumIICase)
+{
+  auto as = GetParam();
+  auto result = s.twoSum(as.nums, as.target);
+  EXPECT_EQ(result, as.result);
+}
+
+INSTANTIATE_TEST_CASE_P(Default, TwoSumIITest,
+    testing::Values(
+      States{{2,7,11,15}, 9, {1, 2}},
+      States{{2, 3, 4}, 6, {1, 3}},
+      States{{-1, 0}, -1, {1, 2}}
+    ));
