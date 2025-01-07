0782e8260cac300218c98091b47aade7362304d4
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-01-07 22:46:59 +0800

    wip: hash set solution

--- solutions/cpp/include/128longest_consecutive_sequence.hpp
+++ solutions/cpp/include/128longest_consecutive_sequence.hpp
@@ -1,56 +1,69 @@
 #pragma once
 
 #include <ranges>
-#include <vector>
+#include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
-class Solution
-{
+class Solution {
 public:
-  int longestConsecutive(std::vector<int>& nums) {
+  int longestConsecutive(std::vector<int> &nums) {
     std::unordered_set<int> records(nums.size());
     records.insert(nums.begin(), nums.end());
 
     int max = 0;
     auto record_size = records.size();
-    for (const auto &record : records)
-    {
-      if (records.find(record - 1) == records.end())
-      {
+    for (const auto &record : records) {
+      if (records.find(record - 1) == records.end()) {
         auto suffix = record + 1;
         int curr_max = 1;
-        while (records.find(suffix) != records.end())
-        {
+        while (records.find(suffix) != records.end()) {
           ++curr_max;
           ++suffix;
         }
         max = std::max(max, curr_max);
       }
     }
-
     return max;
   }
 };
 
-
-class SecondSolution
-{
+// Hash map solution
+class HashMapSolution {
 public:
-  int longestConsecutive(std::vector<int>& nums) {
+  int longestConsecutive(std::vector<int> &nums) {
+    std::unordered_map<int, int> records(nums.size());
+    int max_size = 0;
+    for (const auto &num : nums) {
+      if (!records[num]) {
+        //             left_val + curr_val + right_val
+        records[num] = records[num - 1] + 1 + records[num + 1];
+        // Update left most
+        records[num - records[num - 1]] = records[num];
+        // Update right most
+        records[num + records[num + 1]] = records[num];
+        max_size = std::max(max_size, records[num]);
+      }
+    }
+    return max_size;
+  }
+};
+
+class SecondSolution {
+public:
+  int longestConsecutive(std::vector<int> &nums) {
     std::unordered_set<int> records(nums.size());
     records.insert(nums.begin(), nums.end());
     int length{0};
 
-    for (const auto record : records)
-    {
-      if (records.find(record - 1) == records.end())
-      {
+    for (const auto record : records) {
+      if (records.find(record - 1) == records.end()) {
         int curr_length{0};
         do {
           ++curr_length;
           if (records.find(record + curr_length) == records.end())
             break;
-        }while (true);
+        } while (true);
         // length = std::max(length, curr_length);
         length = (length > curr_length) ? length : curr_length;
       }
@@ -59,36 +72,30 @@ public:
   }
 };
 
-
 class FirstSolution {
 public:
   int longestConsecutive(std::vector<int> &nums) {
     std::unordered_set<int> records(nums.size());
 
-    for (const auto e : nums)
-    {
+    for (const auto e : nums) {
       records.insert(e);
     }
 
     int length{0};
-    for (const auto record : records)
-    {
+    for (const auto record : records) {
       int curr_length = 0;
-      if (auto search = records.find(record - 1); search != records.end())
-      {
+      if (auto search = records.find(record - 1); search != records.end()) {
         continue;
-      }
-      else
-      {
-        do
-        {
+      } else {
+        do {
           ++curr_length;
-          if (auto search = records.find(record + curr_length); search == records.end())
+          if (auto search = records.find(record + curr_length);
+              search == records.end())
             break;
-        }
-        while (true);
+        } while (true);
       }
-      if (curr_length > length) length = curr_length;
+      if (curr_length > length)
+        length = curr_length;
     }
 
     return length;
