6a249f8a8ee931b7dc5540857c124b7163b0f1c3
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-01-07 23:45:01 +0800

    chore: clean up

--- solutions/cpp/include/125valid_palindrome.hpp
+++ solutions/cpp/include/125valid_palindrome.hpp
@@ -29,27 +29,3 @@ public:
     return true;
   }
 };
-
-// Failed...
-// class FirstSolution {
-// public:
-//   bool isPalindrome(std::string s) {
-//     if (s.empty())
-//       return true;
-//
-//     for (auto [first, second] = std::make_tuple(s.begin(), s.rbegin());
-//          first != s.end() && second != s.rend(); ++first, ++second) {
-//       while (!std::isalnum(*first) && std::next(first) != s.end()) {
-//         ++first;
-//       }
-//       while (!std::isalnum(*first) && std::next(second) != s.rend()) {
-//         ++second;
-//       }
-//       std::cout << "first:" << *first << ", second: " << *second << '\n';
-//       if (std::tolower(*first) != std::tolower(*second) ||
-//           first == (second.base() - 1))
-//         return false;
-//     }
-//     return true;
-//   }
-// };
