8331c81aa74b8a85a0a3b86faf53e7e286d83c81
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-02-09 14:48:27 +0800

    style: format it!

--- solutions/cpp/include/496next_greater_element_i.hpp
+++ solutions/cpp/include/496next_greater_element_i.hpp
@@ -8,25 +8,22 @@ class Solution {
 public:
   std::vector<int> nextGreaterElement(std::vector<int> &nums1,
                                       std::vector<int> &nums2) {
-      std::vector<int> res;
+    std::vector<int> res;
     std::unordered_map<int, int> records(nums2.size());
     std::stack<int> stack;
 
-    for (auto rit = nums2.rbegin(); rit != nums2.rend(); ++rit)
-    {
-      while (!stack.empty() && *rit > stack.top())
-      {
+    for (auto rit = nums2.rbegin(); rit != nums2.rend(); ++rit) {
+      while (!stack.empty() && *rit > stack.top()) {
         stack.pop();
       }
       records[*rit] = !stack.empty() ? stack.top() : -1;
       stack.push(*rit);
     }
 
-    for (const auto& num : nums1)
-    {
+    for (const auto &num : nums1) {
       res.push_back(records[num]);
     }
 
     return res;
-}
+  }
 };
