8bf25538bd12bf23384ea6ca0effd32362653b73
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-03-07 22:15:51 +0800

    fix: testing inputs were faulty

--- solutions/cpp/tests/496next_greater_element_i_test.cpp
+++ solutions/cpp/tests/496next_greater_element_i_test.cpp
@@ -15,7 +15,7 @@ struct NextGreaterElementITest : public ::testing::Test,
   Solution s;
 };
 
-TEST_P(NextGreaterElementITest, ValidSudokuCases) {
+TEST_P(NextGreaterElementITest, NextGreaterElementICase) {
   auto as = GetParam();
   auto result = s.nextGreaterElement(as.nums1, as.nums2);
   EXPECT_EQ(result, as.result);
@@ -24,7 +24,7 @@ TEST_P(NextGreaterElementITest, ValidSudokuCases) {
 INSTANTIATE_TEST_SUITE_P(
     Default, NextGreaterElementITest,
     testing::Values(
-      States{{4,1,2}, {1,2,4,2}, {-1,3,-1}},
+      States{{4,1,2}, {1,3,4,2}, {-1,3,-1}},
       States{{2,4},{1,2,3,4},{3,-1}},
       States{{1,3,5,2,4},{6,5,4,3,2,1,7},{7,7,7,7,7}}
    ));
