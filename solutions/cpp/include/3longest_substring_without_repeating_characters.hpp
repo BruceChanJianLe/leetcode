68dca606904630fc06dad401a345def397f07797
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-03-10 22:24:56 +0800

    wip: solve 3 longest substring without repeating characters

---
+++ solutions/cpp/include/3longest_substring_without_repeating_characters.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <iterator>
+#include <string>
+#include <unordered_set>
+#include <deque>
+
+// int method (18ms)
+class Solution {
+public:
+  int lengthOfLongestSubstring(std::string s) {
+      auto left{0}, max_seen{0};
+    std::unordered_set<char> records;
+
+    for (auto right = 0; right < s.size(); ++right) {
+      while (records.count(s[right])) {
+        records.erase(s[left++]);
+      }
+      records.insert(s[right]);
+      max_seen = std::max(max_seen, right - left + 1); // off by one error, when both pointers is pointing as the same element, size is 1;
+    }
+    return max_seen;
+}
+};
+
+// iterator method (7ms)
+class SecondSolution {
+public:
+  int lengthOfLongestSubstring(std::string s) {
+    auto left{s.begin()}, right{s.begin()};
+    std::unordered_set<char> records;
+    auto max_seen{0l};
+
+    while (right != s.end()) {
+      if (!records.insert(*right).second) {
+        bool is_found = false;
+        while (!is_found) {
+          if (*left == *right) {
+            is_found = true;
+          } else {
+            records.erase(*left);
+          }
+          ++left;
+        }
+      }
+      max_seen = std::max(max_seen, std::distance(left, right) + 1);
+      ++right;
+    }
+    return max_seen;
+  }
+};
+
+// 9ms
+class FirstSolution {
+public:
+  int lengthOfLongestSubstring(std::string s) {
+    std::unordered_set<char> records;
+    auto max_seen{0uz};
+    std::deque<char> collection;
+
+    for (const auto& e : s) {
+      if(!records.insert(e).second) {
+        auto is_found = false;
+        while (!is_found) {
+          if (collection.front() == e) {
+            is_found = true;
+          } else {
+            records.erase(collection.front());
+          }
+          collection.pop_front();
+        }
+      }
+      collection.push_back(e);
+      max_seen = std::max(max_seen, collection.size());
+    }
+    return max_seen;
+  }
+};
---
+++ solutions/cpp/tests/3longest_substring_without_repeating_characters_test.cpp
@@ -0,0 +1,45 @@
+#include "3longest_substring_without_repeating_characters.hpp"
+
+#include "gtest/gtest.h"
+
+struct States
+{
+  std::string s;
+  int result;
+};
+
+struct LongestSubstringWithoutRepeatingCharactersTest : public testing::TestWithParam<States>
+{
+  Solution s;
+  SecondSolution ss; // fastest with iterator and while loop
+  FirstSolution fs;
+};
+
+TEST_P(LongestSubstringWithoutRepeatingCharactersTest, LongestSubstringWithoutRepeatingCharactersCase)
+{
+  auto as = GetParam();
+  auto result = s.lengthOfLongestSubstring(as.s);
+  EXPECT_EQ(result, as.result);
+}
+
+TEST_P(LongestSubstringWithoutRepeatingCharactersTest, SecondLongestSubstringWithoutRepeatingCharactersCase)
+{
+  auto as = GetParam();
+  auto result = ss.lengthOfLongestSubstring(as.s);
+  EXPECT_EQ(result, as.result);
+}
+
+TEST_P(LongestSubstringWithoutRepeatingCharactersTest, FirstLongestSubstringWithoutRepeatingCharactersCase)
+{
+  auto as = GetParam();
+  auto result = fs.lengthOfLongestSubstring(as.s);
+  EXPECT_EQ(result, as.result);
+}
+
+INSTANTIATE_TEST_SUITE_P(Default, LongestSubstringWithoutRepeatingCharactersTest,
+    testing::Values(
+      States{"abcabcbb", 3},
+      States{"bbbbb", 1},
+      States{"pwwkew", 3},
+      States{"abba", 2}
+    ));
