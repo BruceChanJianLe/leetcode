de5cf062adb187a861f1094e7398519e0d526600
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-05-25 23:10:15 +0800

    wip: solve 53 maximum subarray

---
+++ solutions/cpp/include/53maximum_subarray.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+// Kadane's algorithm
+// Refer also to maximum subarray product
+class Solution {
+public:
+  int maxSubArray(const std::vector<int>& nums) {
+    // global sum as first element as size is guarantee to be at least 1
+    auto curr_sum{0}, global_sum{nums.front()};
+
+    for (const auto &num : nums) {
+      if (curr_sum > 0) {
+        // Extend the subarray
+        curr_sum += num;
+      } else {
+        // Restart the subarray
+        curr_sum = num;
+      }
+
+      // Update global sum if current one is largest seen
+      global_sum = std::max(global_sum, curr_sum);
+    }
+
+    return global_sum;
+  }
+};
---
+++ solutions/cpp/tests/53maximum_subarray_test.cpp
@@ -0,0 +1,31 @@
+#include "53maximum_subarray.hpp"
+
+#include "gtest/gtest.h"
+
+struct TestCase {
+  std::vector<int> nums;
+  int result;
+};
+
+class MaximumSubarrayTest : public ::testing::TestWithParam<TestCase> {
+protected:
+  Solution s;
+};
+
+TEST_P(MaximumSubarrayTest, HandlesMaxSubarraySum) {
+  auto as = GetParam();
+  auto result = s.maxSubArray(as.nums);
+  EXPECT_EQ(result, as.result);
+}
+
+INSTANTIATE_TEST_SUITE_P(Default, MaximumSubarrayTest,
+  ::testing::Values(
+    TestCase{{-2,1,-3,4,-1,2,1,-5,4}, 6},    // [4,-1,2,1]
+    TestCase{{1}, 1},                        // single element
+    TestCase{{5,4,-1,7,8}, 23},              // full array
+    TestCase{{-1,-2,-3,-4}, -1},             // all negative
+    TestCase{{1,2,3,4,5}, 15},               // all positive
+    TestCase{{-2,-3,4,-1,-2,1,5,-3}, 7},     // [4,-1,-2,1,5]
+    TestCase{{8,-19,5,-4,20}, 21}            // [5,-4,20]
+  )
+);
