614cf457df5385587706b9dd108936d245e89846
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-06-29 23:43:17 +0800

    wip: add top-down example

--- solutions/cpp/include/322coin_change.hpp
+++ solutions/cpp/include/322coin_change.hpp
@@ -69,3 +69,71 @@ private:
     }
   }
 };
+
+class Solution {
+public:
+  int coinChange(const std::vector<int>& coins, int amount) {
+    // std::vector<int> memo(amount + 1, amount + 1);
+    // return coinChangeHelper(coins, amount, memo);
+    // return walk(coins, amount);
+    memo.assign(amount + 1, -2); // -2 mean uncomputed
+    return memoWalk(coins, amount);
+  }
+
+private:
+  std::vector<int> memo;
+
+  int coinChangeHelper(const std::vector<int>& coins, const int amount, std::vector<int>& memo) {
+    if (memo[amount] != amount + 1) return memo[amount];
+    if (amount == 0) return 0;
+
+    for (const auto coin : coins) {
+      // Create subproblem
+      if (auto sub_amount = amount - coin; sub_amount >= 0) {
+        memo[amount] = std::min(memo[amount],
+            coinChangeHelper(coins, sub_amount, memo) + 1);
+      }
+    }
+
+    return (memo[amount] == amount + 1) ? -1 : memo[amount];
+  }
+
+  int walk(const std::vector<int>& coins, const int amount) {
+    if (amount == 0) return 0;
+    if (amount < 0) return -1;
+
+    auto curr_count{std::numeric_limits<int>::max()};
+
+    // Try each coin
+    for (const auto coin : coins) {
+      auto result = walk(coins, amount - coin);
+      if (result != -1) { // only if valid result exist
+        curr_count = std::min(curr_count, result + 1);
+      }
+    }
+
+    return (curr_count == std::numeric_limits<int>::max()) ? -1 : curr_count;
+  }
+
+  int memoWalk(const std::vector<int>& coins, const int amount) {
+    if (amount == 0) return 0;
+    if (amount < 0) return -1;
+
+    if (memo[amount] != -2) return memo[amount];
+
+    auto min_coin{std::numeric_limits<int>::max()};
+
+    // Try each coin
+    for (const auto coin : coins) {
+      auto result = memoWalk(coins, amount - coin);
+      if (result != -1) {
+        min_coin = std::min(min_coin, result + 1);
+      }
+    }
+
+    // Store result in memo
+    memo[amount] = (min_coin == std::numeric_limits<int>::max()) ? -1 : min_coin;
+
+    return memo[amount];
+  }
+};
