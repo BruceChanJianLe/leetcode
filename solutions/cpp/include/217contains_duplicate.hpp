5fb1ea7d0045faf103dddc065b38c60cf9a38b74
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-08-05 23:11:58 +0800

    wip: minor update to 217 solution

--- solutions/cpp/include/217contains_duplicate.hpp
+++ solutions/cpp/include/217contains_duplicate.hpp
@@ -8,7 +8,12 @@ class Solution
 public:
   bool containsDuplicate(std::vector<int> &nums)
   {
+    // Sanity check (based on contraints)
+    if (nums.size() == 1) return false;
+
     std::unordered_set<int> record;
+    record.reserve(nums.size() / 2);
+
     for (auto& num : nums)
     {
       if (!record.insert(num).second) return true;
